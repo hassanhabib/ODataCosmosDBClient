9109a655ad0ec5c909668e8deed1cf20c0f29a94
Hassan Habib <hassanhabib@live.com>
2020-01-05 00:29:34 -0800

    Add project files.

---
+++ StudentsDataClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace StudentsDataClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connectionString = "YOUR_PRIMARY_CONNECTION_STRING_HERE";
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            CloudTable table = tableClient.GetTableReference("students");
+            TableQuery<Student> tableQuery = new TableQuery<Student>();
+            
+            string filter = TableQuery.CombineFilters(
+                TableQuery.GenerateFilterConditionForInt("Score", QueryComparisons.GreaterThan, 150),
+                TableOperators.And,
+                TableQuery.GenerateFilterCondition("Name", QueryComparisons.Equal, "Hassan"));
+            
+            tableQuery.Where(filter);
+
+            tableQuery.OrderBy("Name");
+            tableQuery.Select(new List<string> { "Score" });
+            tableQuery.Take(take: 3);
+
+            TableQuerySegment<Student> studentsSegment = 
+                await table.ExecuteQuerySegmentedAsync(tableQuery, token: null);
+            
+            studentsSegment.Results.ForEach(i => 
+                Console.WriteLine("{0,5} {1,10} {2,-10}", i.PartitionKey, i.Name, i.Score));
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StudentsDataClient/Student.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+
+namespace StudentsDataClient
+{
+    public class Student : TableEntity
+    {
+        public string Name { get; set; }
+        public int? Score { get; set; }
+    }
+}
